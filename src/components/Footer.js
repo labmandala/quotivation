f288a063ac1b86e05d4e1484da21f0f29a23fcd4
sarah <skg@neofilm.us>
2025-01-01 20:46:17 -0500

    Message component & display information to user

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className='wrapper'>&copy; Quotivation</footer>;
+  return <footer className="wrapper">&copy; Quotivation</footer>;
 };
 
 export default Footer;
