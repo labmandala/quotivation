0f52e108a00e3a326d1d40d9ffe145d64bdddec8
sarah <skg@neofilm.us>
2025-01-09 19:29:58 -0500

    Update user on retrieved quotes and details

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,7 +4,9 @@ const Message = ({ messageText, removeMessage }) => {
   return (
     <div className="message">
       <p>{messageText}</p>
-      <span className="close-message" onClick={removeMessage}>X</span>
+      <span className="close-message" onClick={removeMessage}>
+        X
+      </span>
     </div>
   );
 };
