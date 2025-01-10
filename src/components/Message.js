d74445882348bc2b569e8b4637dcf8c61a486f4a
sarah <skg@neofilm.us>
2025-01-09 19:35:12 -0500

    Have messages disappear after delay

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Message = ({ messageText, removeMessage }) => {
+  useEffect(() => {
+    const messageTimeout = window.setTimeout(() => {
+      removeMessage();
+    }, 1500);
+
+    return () => window.clearTimeout(messageTimeout);
+  });
   return (
-    <div className="message">
+    <div className='message'>
       <p>{messageText}</p>
-      <span className="close-message" onClick={removeMessage}>
-        X
+
+      <span className='close-message' onClick={removeMessage}>
+        x
       </span>
     </div>
   );
